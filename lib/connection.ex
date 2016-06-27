1384fa07ae07f1278cca351ac7744ea5b6dd875a
technicalcapt <technicalcapt@gmail.com>
2016-06-27 16:55:49 +0700

    add a connection module

---
+++ lib/connection.ex
@@ -0,0 +1,17 @@
+defmodule Riak.Connection do
+  @doc """
+  Create a linked process to talk with riak server
+  """
+
+  def start_link(host \\ '127.0.0.1', port \\ 8087, args \\ []) do
+    :riakc_pb_socket.start_link(host, port, args)
+  end
+
+  @doc """
+  Create a process to talk with riak server
+  """
+
+  def start(host \\ '127.0.0.1', port \\ 8087, args \\ []) do
+    :riakc_pb_socket.start(host, port, args)
+  end
+end
--- lib/riak.ex
+++ lib/riak.ex
@@ -1,2 +1,18 @@
 defmodule Riak do
+
+  @moduledoc """
+  Riak Time series Elixir client
+  """
+  use :riakc_pb_socket
+
+
+  @doc """
+  Ping the server
+  """
+  def ping(pid) when is_pid(pid), do: :riakc_pb_socket.ping(pid)
+
+  @doc """
+  Put metadata/value in the table
+  """
+
 end
