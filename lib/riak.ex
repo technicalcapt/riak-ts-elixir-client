5ef3df542e2fac17bb2e436ea72ca3cb3beba87e
technicalcapt <technicalcapt@gmail.com>
2016-06-27 17:08:06 +0700

    test put and get data from server

--- lib/riak.ex
+++ lib/riak.ex
@@ -3,8 +3,6 @@ defmodule Riak do
   @moduledoc """
   Riak Time series Elixir client
   """
-  use :riakc_pb_socket
-
 
   @doc """
   Ping the server
@@ -12,7 +10,16 @@ defmodule Riak do
   def ping(pid) when is_pid(pid), do: :riakc_pb_socket.ping(pid)
 
   @doc """
-  Put metadata/value in the table
+  Get data from server
   """
+  def get(pid, table, list \\ [], opts \\ []) when is_binary(table) and is_pid(pid) do
+    :riakc_ts.get(pid, table, list, opts)
+  end
 
+  @doc """
+  Put data to server
+  """
+  def put(pid, table, list \\ []) when is_binary(table) and is_pid(pid) do
+    :riakc_ts.put(pid, table, list)
+  end
 end
