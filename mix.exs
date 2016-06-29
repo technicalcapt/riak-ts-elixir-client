1ce475041163c15fc0e23c20e228d6045b0d1817
technicalcapt <technicalcapt@gmail.com>
2016-06-29 14:49:24 +0700

    use pooler dependency

--- mix.exs
+++ mix.exs
@@ -14,7 +14,7 @@ defmodule Riak.Mixfile do
   #
   # Type "mix help compile.app" for more information
   def application do
-    [applications: [:logger]]
+    [applications: [:pooler]]
   end
 
   # Dependencies can be Hex packages:
@@ -27,6 +27,9 @@ defmodule Riak.Mixfile do
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-    [{:riakc, github: "technicalcapt/riak-erlang-client"}]
+    [
+      {:riakc, github: "technicalcapt/riak-erlang-client"},
+      {:pooler, "~> 1.5"}
+    ]
   end
 end
